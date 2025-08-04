b366afd3aa005efecb37c1376ffbdfcf5ee80371
songyanglin <1503464667@qq.com>
2025-08-04 17:37:23 +0800

    Java学习

---
+++ Learn/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 2147483640;
+        int y = 15;
+        int sum = x + y;
+        System.out.println(sum);
+
+    }
+}
